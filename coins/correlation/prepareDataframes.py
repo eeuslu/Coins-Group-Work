ff8f36d6d5ea911e711b98c8a4c9bc891df18f1a
F. Rehn <fabian96.rehn@gmail.com>
2020-07-07 14:55:13 +0200

    move clustering

--- coins/correlation/prepareDataframes.py
+++ coins/correlation/prepareDataframes.py
@@ -55,27 +55,6 @@ def prepareSocioDemographics(dfSocioDemographics, dropPercentage):
     return dfSocioDemographicsDummies, droppedColumnsList
 
 
-def cluster(dfInput,cluster=5, onlyCluster=False):
-    
-    dfWithId = dfInput.copy()
-
-    if cluster > 0:
-        dfWithoutId = dfWithId.drop('user_id',axis=1)
-
-        kmeans = KMeans(n_clusters=cluster, random_state=0).fit(dfWithoutId)
-
-        prediction = kmeans.predict(dfWithoutId)
-        dfWithId['cluster'] = prediction
-        dfWithId['cluster'] = dfWithId['cluster'].astype('float')
-
-        if onlyCluster == True:
-            for column in dfWithId.columns:
-                if column not in ['user_id','cluster']:
-                    dfWithId.drop(column,axis=1,inplace=True)
-
-    return dfWithId
-
-
 
 # transform values in dfPersonality from real numbers to classes for classification purpose
 def preparePersonality(dfPersonality,train, multiclass=False, split="median"):
